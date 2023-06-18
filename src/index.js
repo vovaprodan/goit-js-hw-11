ddfbbc054e6cabd289bf2b47a4f59d6a01620f96
vovaprodan <121820923+vovaprodan@users.noreply.github.com>
2023-06-18 16:44:58 +0200

    dz

--- src/index.js
+++ src/index.js
@@ -1,44 +1,70 @@
 import {getPhoto} from './server-api'
-const axios = require('axios').default;
+import Notiflix from 'notiflix';
 
 const form = document.querySelector('.search-form');
 const bivEl = document.querySelector('.gallery')
 const buttonEl = document.querySelector('.load-more')
 
+let page = 1;
+let perPage = 40;
+let total = 0;
+
 buttonEl.classList.add('is-hedden')
 
-
-
 form.addEventListener('submit', onSubmitForm)
-buttonEl.addEventListener('click', onClickBtn)
-
-// getPhoto()
-  
+buttonEl.addEventListener('click', onClickBtn) 
 
 function onSubmitForm(evt) { 
+   buttonEl.classList.add('is-hedden')
   evt.preventDefault();
   bivEl.innerHTML = '';
-  
   input = evt.currentTarget.elements.searchQuery.value
-   form.reset();
-  
-  getPhoto(input)
- 
-}
 
+  if (input === '') {
+   return 
+  }
+  form.reset();
+ page = 1
+
+  getPhoto(input, page).then(data => {
+    const photo = data.hits
+    if (photo.length === 0) {
+      Notiflix.Notify.failure('Sorry there are no images matching your search query. Please try again.');
+    }
+    getMarkup(photo)
+      
+    buttonEl.classList.remove('is-hedden')
+  }) 
+}
 function onClickBtn() {
+ 
+  page += 1
+  total = page * perPage
+  console.log(total)
+  getPhoto(input, page).then(data => {
+    const totalHits = data.totalHits
+    const photo = data.hits
+    getMarkup(photo)
 
-  getPhoto(input)
+    if (total > totalHits) {
+      buttonEl.classList.add('is-hedden')
+      Notiflix.Notify.failure('Sorry there are no images matching your search query. Please try again.');
+    }
+  })
 }
 
-
-//   async function getPhoto() {
-//   try {
-//     const response = await axios.get(`https://pixabay.com/api/?key=37294582-8910eff478423fe25551a6b37&image_type=photo&orientation=horizontal&safesearch=true`);
-//     const data = await response.json()
-//     console.log(response);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
+function getMarkup(photo) {
+photo.map((photo) => {
+        const markup = `<div class="photo-card"><img src="${photo.webformatURL}"alt="${photo.tags}"loading="lazy"width='320'/>
+          <div class="info"><p class="info-item"><b>Likes</b>${photo.likes}</p><p class="info-item"><b>Views</b>${photo.views}</p>
+    <p class="info-item">
+      <b>Comments</b>${photo.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${photo.downloads}
+    </p>
+  </div>
+</div>`
+        bivEl.insertAdjacentHTML("beforeend", markup);
+      })
+}
